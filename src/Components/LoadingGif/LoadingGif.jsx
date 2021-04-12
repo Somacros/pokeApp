4e80919c07c4533ab56e85163c28ce94ade9f57c
Somacros <marcoseduardo2604@gmail.com>
2021-04-12 10:44:34 -0600

    Added Loading text to Loading gif

--- src/Components/LoadingGif/LoadingGif.jsx
+++ src/Components/LoadingGif/LoadingGif.jsx
@@ -5,6 +5,9 @@ const LoadingPokeball = () => {
     return (
         <div className="tc">
             <img src={ Pokeball } className="h-5" alt="" />
+            <div>
+                <p className="helvetica mt0 di ml0 pa0 fw6 f5-l f6 tc">Loading...</p>
+            </div>
         </div>
     );
 }
